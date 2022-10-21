ce375803ddf3f18c0e87eba23c261f224d3e0073
Allen Huang <allenhuang@Allens-MacBook-Pro.local>
2022-10-21 16:08:00 -0400

    YEET

--- benchmark/benchmark.go
+++ benchmark/benchmark.go
@@ -1,1 +1,22 @@
 package benchmark
+
+import "go-learned-indexes/indexes"
+
+var (
+	outputCSVDir = "./results"
+	datasetDir = "./data"
+)
+
+func LoadDataset(filename string) *[]indexes.KeyValue {
+	// TRINITY do this
+	// read a dataset from disk, read the values, and load it in
+	return nil
+}
+
+func RunAllIndexes() {
+	// build all indexes and run them
+}
+
+func RunIndex(index indexes.SecondaryIndex, sizeScale int64) {
+	bound := index.Lookup()
+}
