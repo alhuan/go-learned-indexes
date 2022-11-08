1bc2113274472ad4ac266c3c4a8ea992a2693e01
Trinity Gao <trinityg@mit.edu>
2022-11-08 16:40:31 -0500

    Basic implementation of LoadDataset

--- benchmark/utils.go
+++ benchmark/utils.go
@@ -1,15 +1,49 @@
 package benchmark
 
 import (
+	"bufio"
+	"errors"
 	"go-learned-indexes/indexes"
+	"log"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 // TRINITY do these two functions
 
-func LoadDataset(filename string) *[]indexes.KeyValue {
+func LoadDataset(filename string) (*[]indexes.KeyValue, error) {
 	// read a dataset from disk, read the values, and load it in
-	return nil
+	f, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	data := []indexes.KeyValue{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		tokens := strings.Fields(text)
+		if len(tokens) != 2 { // Assuming key value on each line
+			return nil, errors.New("Line had more than 2 tokens")
+		}
+		intKey, _ := strconv.ParseInt(tokens[0], 0, 64)
+		intValue, _ := strconv.ParseInt(tokens[1], 0, 64)
+		item := indexes.KeyValue{Key: intKey, Value: intValue}
+		data = append(data, item)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	ptr := &data
+	return ptr, nil
 }
 func BinarySearch(data *[]indexes.KeyValue, lookupKey int64, bound indexes.SearchBound) int64 {
 	// model the binary search off of the function body of sort.Search(), but we should probably use int64s instead of int32s
