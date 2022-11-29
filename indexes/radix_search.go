2f578212c408785c418e64e02f8edf51e905a0fb
Alexandra Li <alexandrali766@gmail.com>
2022-11-29 17:47:02 -0500

    maybe finish radix search

--- indexes/radix_search.go
+++ indexes/radix_search.go
@@ -1,96 +1,98 @@
 package indexes
 
 import (
-	"math/bits"
+	"math"
 	"unsafe"
 )
 
 type Coord struct {
-	x 	uint64
-	y 	float64
+	x uint64
+	y float64
 }
 
 type RadixSearch struct {
-	minKey 				uint64
-	maxKey 				uint64
-	numKeys 			uint64
-	numRadixBits 		uint64
-	numShiftBits 		uint64
-	maxError 			uint64
+	minKey       uint64
+	maxKey       uint64
+	numKeys      uint64
+	numRadixBits uint64
+	numShiftBits uint64
+	maxError     uint64
 
-	radixTable 			[]uint64
-	splinePoints 		[]Coord
+	radixTable   []uint64
+	splinePoints []Coord
 
-
-	// maybe don't need this if all the building can be done in one function?
-	curNumKeys 			uint64
-	curNumDistinctKeys	uint64
-	prevKey 			uint64
-	prevPosition 		uint64
-	prevPrefix 			uint64
-
-	upperLimit 			Coord
-	lowerLimit 			Coord
-	prevPoint 			Coord
+	prevPrefix uint64
 }
 
 func (r *RadixSearch) Lookup(key uint64) SearchBound {
-	estimate := getEstimatedPosition(key)
-	begin := (estimate < r.maxError) ? 0 : (estimate - r.maxError) // AGAIN DOES GO HAVE TERNARY
-	end := (estimate + r.maxError + 2 > r.numKeys) ? r.numKeys : (estimate + r.maxError + 2)
-    return SearchBound{begin, end}
+	estimate := r.getEstimatedPosition(key)
+	var begin uint64
+	var end uint64
+	if estimate < r.maxError {
+		begin = 0
+	} else {
+		begin = estimate - r.maxError
+	}
+	if estimate+r.maxError+2 > r.numKeys {
+		end = r.numKeys
+	} else {
+		end = estimate + r.maxError + 2
+	}
+	return SearchBound{begin, end}
 }
 
-func (r *RadixSearch) getEstimatedPosition(key uint64) int {
-    if (key <= r.minKey) {
+func (r *RadixSearch) getEstimatedPosition(key uint64) uint64 {
+	if key <= r.minKey {
 		return 0
 	}
-    if (key >= r.maxKey) {
+	if key >= r.maxKey {
 		return r.numKeys - 1
 	}
 
-	index := getSplineSegment(key)
+	index := r.getSplineSegment(key)
 
-    down := r.splinePoints[index - 1]
-    up := r.splinePoints[index]
+	down := r.splinePoints[index-1]
+	up := r.splinePoints[index]
 
-    x_diff := up.x - down.x
-    y_diff := up.y - down.y
-    slope := y_diff / x_diff
-
-    // Interpolate.
-    key_diff := key - down.x
-    return std::fma(key_diff, slope, down.y) //TODO: WHAT IS FMA IN GO ?? and in c++ lol 
+	xDiff := float64(up.x - down.x)
+	yDiff := up.y - down.y
+	slope := yDiff / xDiff
+	keyDiff := float64(key - down.x)
+	return uint64(math.FMA(keyDiff, slope, down.y)) // x * y + z, computed with only one rounding.
 }
 
-func getSplineSegment(key uint64) int {
-	prefix := = (key - r.minKey) >> r.numShiftBits
+func (r *RadixSearch) getSplineSegment(key uint64) uint64 {
+	prefix := (key - r.minKey) >> r.numShiftBits
 	begin := r.radixTable[prefix]
-	end := r.radixTable[prefix + 1]
+	end := r.radixTable[prefix+1]
 
-    if (end - begin < 32) {
-    	current := begin
-		while (r.splinePoints[current].x < key) {
-			current ++
+	if end-begin < 32 {
+		current := begin
+		for r.splinePoints[current].x < key {
+			current++
 		}
 		return current
-    }
+	}
 
-
-	//TODO::: ER I HAVE NO IDEA HOW TO CHANGE THIS PART LOL
-	const auto lb = std::lower_bound(
-        spline_points_.begin() + begin, spline_points_.begin() + end, key,
-        [](const Coord<KeyType>& coord, const KeyType key) {
-          return coord.x < key;
-        });
-    return std::distance(spline_points_.begin(), lb);
-  }
+	count := end - begin + 1 //is it inclusive?
+	var it uint64
+	for count > 0 {
+		it = begin
+		step := count / 2
+		it += step
+		if r.splinePoints[it].x < key {
+			it++
+			begin = it
+			count -= step + 1
+		} else {
+			count = step
+		}
+	}
+	return begin
+}
 
 func (r *RadixSearch) Size() int64 {
-	return int64(unsafe.Sizeof(*r))
-
-	//TODO :::: THIS NEEDS TO CHANGE BUT IDK WHAT SIZE OF IS LOL
-	return unsafe.Sizeof(*r) + r.radixTable.size() * sizeof(uint32_t) + r.splinePoints.size() * sizeof(Coord<uint32>)
+	return int64(unsafe.Sizeof(*r)) + int64(len(r.radixTable)*8+len(r.splinePoints)*16)
 }
 
 func (r *RadixSearch) Name() string {
@@ -99,142 +101,152 @@ func (r *RadixSearch) Name() string {
 
 func NewRadixSearch(data *[]KeyValue, numRadixBits uint64, maxError uint64) SecondaryIndex {
 	n := len(*data)
-
 	rs := &RadixSearch{}
+	var curNumKeys uint64
+	var curNumDistinctKeys uint64
+	var prevKey uint64
+	var prevPosition uint64
+	var upperLimit Coord
+	var lowerLimit Coord
+	var prevPoint Coord
 
 	rs.minKey = (*data)[0].Key
 	rs.maxKey = (*data)[n-1].Key
-    max_prefix := (rs.maxKey - rs.minKey) >> numRadixBits
-	rdx.radixTable = make([]uint64, max_prefix + 2) //how to fill this with all 0's?
+	maxPrefix := (rs.maxKey - rs.minKey) >> numRadixBits
+	rs.radixTable = make([]uint64, maxPrefix+2) //how to fill this with all 0's?
+	for i := 0; uint64(i) < maxPrefix+2; i++ {  // ^ sus
+		rs.radixTable[i] = 0
+	}
 
 	for i := 0; i < n; i++ {
 		curKey := (*data)[i].Key
-
-		// L O L IS POSITION I ???????
+		pos := float64(i)
+		maxErrorF := float64(maxError)
 
 		//AddKey {
 		//PossiblyAddKeyToSpline {
-		if (curNumKeys == 0) {
-			rs.addKeyToSpline(curKey, i)
-			curNumDistinctKeys++;
+		if curNumKeys == 0 {
+			rs.addKeyToSpline(curKey, pos)
+			curNumDistinctKeys++
 
-			//RememberPreviousCDFPoint 
-			prevPoint = Coord{curKey, i};
+			//RememberPreviousCDFPoint
+			prevPoint = Coord{curKey, pos}
 			continue
 		}
-	
-		if (curKey == prevKey) {
+
+		if curKey == prevKey {
 			continue
 		}
-	
 		curNumDistinctKeys++
 
-		if (curNumDistinctKeys == 2) {
-			upperLimit = Coord{curKey, i}
-			lowerLimit = Coord{curKey, (position < rs.maxError) ? 0 : position - rs.maxError}
-			prevPoint = Coord{curKey, i}
+		if curNumDistinctKeys == 2 {
+			upperLimit = Coord{curKey, pos}
+			if pos < maxErrorF {
+				lowerLimit = Coord{curKey, 0}
+			} else {
+				lowerLimit = Coord{curKey, pos - maxErrorF}
+			}
+			prevPoint = Coord{curKey, pos}
 			continue
 		}
-	
-		last := splinePoints[len(splinePoints) - 1] // IDK DOES THIS DO :: const Coord<KeyType>& last = spline_points_.back();
 
-		upper_y := i + maxError
-		lower_y := (i < maxError) ? 0 : i - maxError //DOES go have ternary?????
+		last := rs.splinePoints[len(rs.splinePoints)-1]
 
-		upper_limit_x_diff := upperLimit.x - last.x
-		lower_limit_x_diff := lowerLimit.x - last.x
-		x_diff := curKey - last.x
-		upper_limit_y_diff := upperLimit.y - last.y
-		lower_limit_Y_diff := lowerLimit.y - last.y
-		y_diff := i - last.y
-
-
-
-		if ((ComputeOrientation(upper_limit_x_diff, upper_limit_y_diff, x_diff, y_diff) != Orientation::CW) || //fix for golang enums
-			(ComputeOrientation(lower_limit_x_diff, lower_limit_y_diff, x_diff,y_diff) != Orientation::CCW)) {
-			rs.addKeyToSpline(prevPoint.x, prevPoint.y) //idk if the types will match up for y
-		
-			upperLimit = Coord{curKey, upper_y}
-			lowerLimit = Coord{curKey, lower_y}
+		upperY := pos + maxErrorF
+		var lowerY float64
+		if pos < maxErrorF {
+			lowerY = 0
 		} else {
-			upper_y_diff := upper_y - last.y
+			lowerY = pos - maxErrorF
+		}
 
-			if (ComputeOrientation(upper_limit_x_diff, upper_limit_y_diff, x_diff,upper_y_diff) == Orientation::CW) {
-				upperLimit = Coord{curKey, upper_y}
+		upperLimitXDiff := float64(upperLimit.x - last.x)
+		lowerLimitXDiff := float64(lowerLimit.x - last.x)
+		xDiff := float64(curKey - last.x)
+		upperLimitYDiff := upperLimit.y - last.y
+		lowerLimitYDiff := lowerLimit.y - last.y
+		yDiff := pos - last.y
+
+		if computeOrientation(upperLimitXDiff, upperLimitYDiff, xDiff, yDiff) != CW || computeOrientation(lowerLimitXDiff, lowerLimitYDiff, xDiff, yDiff) != CCW {
+			rs.addKeyToSpline(prevPoint.x, prevPoint.y)
+
+			upperLimit = Coord{curKey, upperY}
+			lowerLimit = Coord{curKey, lowerY}
+		} else {
+			upperYDiff := upperY - last.y
+			if computeOrientation(upperLimitXDiff, upperLimitYDiff, xDiff, upperYDiff) == CW {
+				upperLimit = Coord{curKey, upperY}
 			}
-	
-			lower_y_diff := lower_y - last.y
-			if (ComputeOrientation(lower_limit_x_diff, lower_limit_y_diff, x_diff,lower_y_diff) == Orientation::CCW) {
-				lowerLimit = Coord{curKey, lower_y}
+			lowerYDiff := lowerY - last.y
+			if computeOrientation(lowerLimitXDiff, lowerLimitYDiff, xDiff, lowerYDiff) == CCW {
+				lowerLimit = Coord{curKey, lowerY}
 			}
 		}
-	
+
 		//RememberPreviousCDFPoint(key, position);
-		prevPoint = Coord{curKey, i}
-
+		prevPoint = Coord{curKey, pos}
 		// } end of PossiblyAddKeyToSpline
-		
-		curNumKeys++
-		prevKey = key
-		prevPosition = position
 
+		curNumKeys++
+		prevKey = curKey
+		prevPosition = uint64(i)
 		// } end of AddKey
 	}
 
-
 	//RadixSpline<KeyType> Finalize() {
-    if (curNumKeys > 0 && rs.splinePoints.back().x != prevKey) { //TODO: CHANGE the .back to get the last element in golang
-		addKeyToSpline(prevKey, prevPosition)
+	if curNumKeys > 0 && rs.splinePoints[len(rs.splinePoints)-1].x != prevKey {
+		rs.addKeyToSpline(prevKey, float64(prevPosition))
 	}
 
-    // FinalizeRadixTable(); {
-	prevPrefix++
-    for (; prevPrefix < len(rs.radixTable); prevPrefix++) {
-      rs.radixTable[prevPrefix] = len(rs.splinePoints)
+	// FinalizeRadixTable(); {
+	rs.prevPrefix++
+	for ; rs.prevPrefix < uint64(len(rs.radixTable)); rs.prevPrefix++ {
+		rs.radixTable[rs.prevPrefix] = uint64(len(rs.splinePoints))
 	}
-	
+
 	// } end of FinalizeRadixTable()
 
+	// i don't think i need to make a copy because go doesn't pass by reference?
 
-	// DOES THIS MATTER ??? should i make deep copies of spline points and radix table ???
-    // return RadixSpline<KeyType>(
-    //     min_key_, max_key_, curr_num_keys_, num_radix_bits_, num_shift_bits_,
-    //     max_error_, std::move(radix_table_), std::move(spline_points_));
+	// return RadixSpline<KeyType>(
+	//     min_key_, max_key_, curr_num_keys_, num_radix_bits_, num_shift_bits_,
+	//     max_error_, std::move(radix_table_), std::move(spline_points_));
 
-
-	// end of Finalize } 
+	// end of Finalize }
 
 	return rs
 }
 
-
-func (r *RadixSearch) addKeyToSpline(curKey uint64, i int) { //IS THIS HOW TO MAKE VOID FUNCTION?
+func (r *RadixSearch) addKeyToSpline(curKey uint64, i float64) {
 	r.splinePoints = append(r.splinePoints, Coord{curKey, i}) //is this super inefficient??
-
 	// PossiblyAddKeyToRadixTable(key) {
-	curPrefix := (curKey - rs.minKey) >> rs.numShiftBits
-	if (curPrefix != rs.prevPrefix) {
-		curIndex := len(rs.splinePoints) - 1
-		for pref := rs.prevPrefix + 1; pref <= curPrefix; pref++ {
-			rs.radixTable[pref] = curIndex
+	curPrefix := (curKey - r.minKey) >> r.numShiftBits
+	if curPrefix != r.prevPrefix {
+		curIndex := len(r.splinePoints) - 1
+		for pref := r.prevPrefix + 1; pref <= curPrefix; pref++ {
+			r.radixTable[pref] = uint64(curIndex)
 		}
-		rs.prevPrefix = curPrefix
+		r.prevPrefix = curPrefix
 	}
 	// }end of PossiblyAddKeyToRadixTable
 }
 
+type Orientation int
 
+const (
+	Collinear Orientation = 0
+	CW                    = 1
+	CCW                   = 2
+)
 
-
-//TODO: CHANGE THIS WHOLE THING TO GO LOL DOES GOLANG HAVE ENUMS HOPEFULLy
-enum Orientation { Collinear, CW, CCW };
-static constexpr double precision = std::numeric_limits<double>::epsilon();
-
-static Orientation ComputeOrientation(const double dx1, const double dy1, const double dx2, const double dy2) {
-	const double expr = std::fma(dy1, dx2, -std::fma(dy2, dx1, 0));
-	if (expr > precision)
-		return Orientation::CW;
-	else if (expr < -precision)
-		return Orientation::CCW;
-	return Orientation::Collinear;
-};
+func computeOrientation(dx1 float64, dy1 float64, dx2 float64, dy2 float64) Orientation {
+	expr := math.FMA(dy1, dx2, -math.FMA(dy2, dx1, 0))
+	precision := math.Nextafter(1, 2) - 1
+	if expr > precision {
+		return CW
+	} else if expr < -precision {
+		return CCW
+	} else {
+		return Collinear
+	}
+}
