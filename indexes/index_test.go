45682a935ae06a42b82ddd2664900f5c431ba3b6
Alexandra Li <alexandrali766@gmail.com>
2022-12-09 19:28:26 -0500

    Index test

---
+++ indexes/index_test.go
@@ -0,0 +1,49 @@
+package indexes
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	// FILL IN INDEX HERE
+	var index SecondaryIndex
+
+	intSet := make(map[uint64]struct{})
+	for len(intSet) < 10_000 {
+		randomInt := rand.Uint64()
+		if _, ok := intSet[randomInt]; ok {
+			continue
+		}
+		intSet[randomInt] = struct{}{}
+	}
+
+	var intList []uint64
+	for k, _ := range intSet {
+		intList = append(intList, k)
+	}
+	sort.Slice(intList, func(i, j int) bool { return intList[i] < intList[j] })
+
+	var keysList []KeyValue
+	for i, uint := range intList {
+		keysList = append(keysList, KeyValue{Key: uint, Value: uint64(i)})
+	}
+
+	// PUT CONSTRUCTION FUNCTION HERE I GUESS?
+	index = NewRadixSpline(&keysList, 20, 200)
+
+	for _, keyValue := range keysList {
+		searchRange := index.Lookup(keyValue.Key)
+		found := false
+		// linear search because im lazy
+		for i := searchRange.Start; i < searchRange.Stop; i++ {
+			if keysList[i].Key == keyValue.Key {
+				found = true
+			}
+		}
+		if !found {
+			t.Fatal("Not found")
+		}
+	}
+}
