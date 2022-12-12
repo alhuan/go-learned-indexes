bd0209cbd39e9fcc1514a2f72ef193ebd14aba04
Giorgi Kldiashvili <giorgik@mit.edu>
2022-12-12 04:04:34 -0500

    Added error bounds to rmi.

--- indexes/rmi_index.go
+++ indexes/rmi_index.go
@@ -3,17 +3,27 @@ package indexes
 import "math"
 import "unsafe"
 
+func clamp(val int64, lo int64, hi int64) int64{
+	if val < lo {
+		return lo
+	}
+	if hi < val {
+		return hi
+	}
+	return val
+}
+
 // do a lookup by using layer1weight to derive the correct layer2 weight, then use the layer2 weight
 // to perform a guess on positional information and bound it by max error bounds
 // there's a bit of work to be done in figuring out which error bounds to use so whoever's doing this
 // better read their papers good
 type RMIIndex struct {
-	maxError		uint64
-
 	n_keys			int64
 	layer2_size 	int64
 	l1				Model
 	l2				[]Model
+
+	errors			[]int64
 }
 
 func NewRMIIndex[Layer1 Model, Layer2 Model](keyValues *[]KeyValue, layer2_size int64) SecondaryIndex {
@@ -21,7 +31,6 @@ func NewRMIIndex[Layer1 Model, Layer2 Model](keyValues *[]KeyValue, layer2_size 
 
 	rmi.layer2_size = layer2_size
 	rmi.n_keys = int64(len(*keyValues))
-	rmi.maxError = 10
 
 	// Train layer1 with compression.
 	var l1 Layer1
@@ -29,18 +38,16 @@ func NewRMIIndex[Layer1 Model, Layer2 Model](keyValues *[]KeyValue, layer2_size 
 	rmi.l1 = l1
 
 	// Train layer2 models.
-	var segment_id, segment_start int64
-	segment_start = 0
-	segment_id = 0
+	var segment_start int64 = 0
+	var segment_id int64 = 0
 	for pos, val := range *keyValues {
-		var i, pred_segment_id int64
-		i = int64(pos)
-		pred_segment_id = rmi.getSegmentId(val.Key)
+		var i int64 = int64(pos)
+		var pred_segment_id int64 = rmi.getSegmentId(val.Key)
 		if pred_segment_id > segment_id {
 			var l2 Layer2
 			l2.Train((*keyValues)[segment_start: i], segment_start, 1)
 			rmi.l2 = append(rmi.l2, l2)
-			for j := segment_id; j < pred_segment_id; j ++ {
+			for j := segment_id + 1; j < pred_segment_id; j ++ {
 				var newl2 Layer2
 				newl2.Train((*keyValues)[i - 1: i], i - 1, 1)
 				rmi.l2 = append(rmi.l2, newl2)
@@ -60,6 +67,21 @@ func NewRMIIndex[Layer1 Model, Layer2 Model](keyValues *[]KeyValue, layer2_size 
 		rmi.l2 = append(rmi.l2, newl2)
 	}
 
+	rmi.errors = make([]int64, layer2_size)
+
+	// Compute local abosolute error bounds.
+	for pos, val := range *keyValues {
+		var i, segment_id int64
+		i = int64(pos)
+		segment_id = rmi.getSegmentId(val.Key)
+		pred := clamp(int64(rmi.l2[segment_id].Predict(val.Key)), 0, rmi.n_keys - 1)
+		if pred > i {
+			rmi.errors[segment_id] = int64(math.Max(float64(rmi.errors[segment_id]), float64(pred - i)))
+		} else {
+			rmi.errors[segment_id] = int64(math.Max(float64(rmi.errors[segment_id]), float64(i - pred)))
+		}
+	}
+
 	return rmi
 }
 
@@ -70,10 +92,12 @@ func (rmi *RMIIndex) getSegmentId(key uint64) int64 {
 }
 
 func (rmi *RMIIndex) Lookup(key uint64) SearchBound {
-	prediction := rmi.l2[rmi.getSegmentId(key)].Predict(key)
-	var index uint64 
-	index = uint64(math.Max(math.Min(prediction, float64(rmi.n_keys - 1)), 0))
-	return SearchBound{index - rmi.maxError, index + rmi.maxError}
+	segment_id := rmi.getSegmentId(key)
+	prediction := int64(rmi.l2[segment_id].Predict(key))
+	var lo uint64 = uint64(clamp(prediction - rmi.errors[segment_id], 0, rmi.n_keys - 1))
+	var hi uint64 = uint64(clamp(prediction + rmi.errors[segment_id], 0, rmi.n_keys - 1))
+	// FIXME: add bounds
+	return SearchBound{lo, hi}
 }
 
 func (rmi *RMIIndex) Size() int64 {
