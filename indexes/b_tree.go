886774612dc2606db1e343ce444898e029eafecb
Allen Huang <allenhuang@Allens-MacBook-Pro.local>
2022-12-13 22:53:41 -0500

    gus

--- indexes/b_tree.go
+++ indexes/b_tree.go
@@ -47,7 +47,8 @@ func (B *BTreeSecondaryIndex) Name() string {
 }
 
 func NewBtreeIndex(keyValues *[]KeyValue, gap uint64) SecondaryIndex {
-	var tree = btree.NewMap[uint64, uint64](4)
+	var tree = btree.NewMap[uint64, uint64](0)
+
 	for i := 0; i < len(*keyValues); i += int(gap) { //load in every gap size element
 		var curKeyVal = (*keyValues)[i]
 		tree.Load(curKeyVal.Key, curKeyVal.Value)
